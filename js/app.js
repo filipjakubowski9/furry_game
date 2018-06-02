6743b1005c9be3be54c4e04b455b1e41c30395eb
Filip Jakubowski <filipjakubowski9@gmail.com>
2018-06-02 23:21:15 +0200

    startGame() + moveFurry() + setInterval() + hideVisibleFurry()

--- js/app.js
+++ js/app.js
@@ -23,15 +23,48 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     Game.prototype.showFurry = function() {
+        this.hideVisibleFurry();
         this.board[this.index(this.furry.x, this.furry.y)].classList.add('furry');
+    }
+
+    Game.prototype.hideVisibleFurry = function() {
+        var clone = document.querySelector('.furry');
+        if (clone) {
+            clone.classList.remove('furry');
+        }
     }
 
     Game.prototype.showCoin = function() {
         this.board[this.index(this.coin.x, this.coin.y)].classList.add('coin');
     }
 
+    Game.prototype.moveFurry = function() {
+        if(this.furry.direction === 'right') {
+            this.furry.x = this.furry.x + 1;
+        }
+        if(this.furry.direction === 'left') {
+            this.furry.x = this.furry.x - 1;
+        }
+        if(this.furry.direction === 'down') {
+            this.furry.y = this.furry.y + 1;
+        }
+        if(this.furry.direction === 'up') {
+            this.furry.y = this.furry.y - 1;
+        }
+        this.showFurry();
+    }
+
+    Game.prototype.startGame = function() {
+        var self = this;
+        this.idSetInterval = setInterval(function() {
+            // console.log("wooohoo");
+            self.moveFurry();
+        }, 250);
+    }
+
     var game = new Game();
     game.showFurry();
     game.showCoin();
+    game.startGame();
 
 });
